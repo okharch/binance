809be5e052b60e751f00987d5e20947716fa8a8b
Oleksandr Kharchenko <okharch@gmail.com>
2023-03-28 17:16:19 +0300

    CREATE TABLE binance.kline_period

--- klines/create_klines_tables.sql
+++ klines/create_klines_tables.sql
@@ -43,3 +43,26 @@ BEGIN
 
 END
 $$;
+
+CREATE TABLE binance.kline_periods (
+                                       period VARCHAR(4) PRIMARY KEY,
+                                       duration_seconds INT
+);
+
+INSERT INTO binance.kline_periods (period, duration_seconds)
+VALUES
+    ('1m', 60),
+    ('3m', 180),
+    ('5m', 300),
+    ('15m', 900),
+    ('30m', 1800),
+    ('1h', 3600),
+    ('2h', 7200),
+    ('4h', 14400),
+    ('6h', 21600),
+    ('8h', 28800),
+    ('12h', 43200),
+    ('1d', 86400),
+    ('3d', 259200),
+    ('1w', 604800);
+
