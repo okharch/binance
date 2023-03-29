c03f5daac13b8c8f282daec15781ca43123743c4
Oleksandr Kharchenko <okharch@gmail.com>
2023-03-29 18:21:38 +0300

    Create klines table and add comments on table and columns

--- klines/create_klines_tables.sql
+++ klines/create_klines_tables.sql
@@ -1,68 +1,71 @@
 create schema if not exists binance;
-CREATE OR REPLACE PROCEDURE binance.create_klines_table( aperiod TEXT )
-    LANGUAGE plpgsql
-AS $$
-BEGIN
-    -- Construct the CREATE TABLE statement with the appropriate time interval
-    -- Valid values are: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M.
-    EXECUTE format($STM$
-        CREATE TABLE IF NOT EXISTS klines_%s (
-            symbol VARCHAR(20) NOT NULL,
-            open_time BIGINT NOT NULL, -- The timestamp of the start time of the kline/candlestick data point, in milliseconds since the Unix epoch.
-            open_price NUMERIC(18, 8), -- The opening price of the trading pair during this interval.
-            high_price NUMERIC(18, 8), -- The highest price of the trading pair during this interval.
-            low_price NUMERIC(18, 8), -- The lowest price of the trading pair during this interval.
-            close_price NUMERIC(18, 8), -- The closing price of the trading pair during this interval.
-            volume NUMERIC(28, 8), -- The trading volume during this interval.
-            close_time BIGINT, -- The timestamp of the end time of the kline/candlestick data point, in milliseconds since the Unix epoch.
-            quote_asset_volume NUMERIC(28, 8), -- The total value of the trading volume in the quote asset, which is the second asset in the trading pair (in this case, USDT).
-            num_trades BIGINT, -- The number of trades that occurred during this interval.
-            taker_buy_base_asset_volume NUMERIC(28, 8), -- The total amount of the base asset (in this case, BTC) that was bought by taker trades during this interval.
-            taker_buy_quote_asset_volume NUMERIC(28, 8), -- The total value of the base asset volume in the quote asset (USDT) that was bought by taker trades during this interval.
-            PRIMARY KEY (symbol, open_time)
-        )
-    $STM$,aperiod);
+CREATE TABLE IF NOT EXISTS binance.klines (
+                                      symbol VARCHAR(20) NOT NULL, -- The trading pair symbol, e.g. BTCUSDT
+                                      period varchar(4) not null, -- Valid values are: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M.
+                                      open_time BIGINT NOT NULL, -- The timestamp of the start time of the kline/candlestick data point, in milliseconds since the Unix epoch.
+                                      open_price NUMERIC(18, 8), -- The opening price of the trading pair during this interval.
+                                      high_price NUMERIC(18, 8), -- The highest price of the trading pair during this interval.
+                                      low_price NUMERIC(18, 8), -- The lowest price of the trading pair during this interval.
+                                      close_price NUMERIC(18, 8), -- The closing price of the trading pair during this interval.
+                                      volume NUMERIC(28, 8), -- The trading volume during this interval.
+                                      close_time BIGINT, -- The timestamp of the end time of the kline/candlestick data point, in milliseconds since the Unix epoch.
+                                      quote_asset_volume NUMERIC(28, 8), -- The total value of the trading volume in the quote asset, which is the second asset in the trading pair (in this case, USDT).
+                                      num_trades BIGINT, -- The number of trades that occurred during this interval.
+                                      taker_buy_base_asset_volume NUMERIC(28, 8), -- The total amount of the base asset (in this case, BTC) that was bought by taker trades during this interval.
+                                      taker_buy_quote_asset_volume NUMERIC(28, 8), -- The total value of the base asset volume in the quote asset (USDT) that was bought by taker trades during this interval.
+                                      PRIMARY KEY (symbol, period, open_time)
+);
+    COMMENT ON SCHEMA binance IS 'Schema containing tables for Binance exchange data.';
 
-    EXECUTE format('COMMENT ON SCHEMA binance IS ''Schema containing tables for Binance exchange data.'';');
-    EXECUTE format('COMMENT ON TABLE binance.klines_%s IS ''Kline/candlestick data for the %s time interval.'';', aperiod, aperiod);
-
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.symbol IS ''The trading pair symbol, e.g. BTCUSDT.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.open_time IS ''The timestamp of the start time of the kline/candlestick data point, in milliseconds since the Unix epoch.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.open_price IS ''The opening price of the trading pair during this interval.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.high_price IS ''The highest price of the trading pair during this interval.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.low_price IS ''The lowest price of the trading pair during this interval.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.close_price IS ''The closing price of the trading pair during this interval.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.volume IS ''The trading volume during this interval.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.close_time IS ''The timestamp of the end time of the kline/candlestick data point, in milliseconds since the Unix epoch.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.quote_asset_volume IS ''The total value of the trading volume in the quote asset, which is the second asset in the trading pair (in this case, USDT).'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.num_trades IS ''The number of trades that occurred during this interval.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.taker_buy_base_asset_volume IS ''The total amount of the base asset (in this case, BTC) that was bought by taker trades during this interval.'';', aperiod);
-    EXECUTE format('COMMENT ON COLUMN binance.klines_%s.taker_buy_quote_asset_volume IS ''The total value of the base asset volume in the quote asset (USDT) that was bought by taker trades during this interval.'';', aperiod);
+    COMMENT ON TABLE binance.klines IS 'Kline/candlestick bars for a trading pair, fetched from the Binance API.';
+    COMMENT ON COLUMN binance.klines.period IS 'Valid values are: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w'; -- ignore 1M
+    COMMENT ON COLUMN binance.klines.symbol IS 'The trading pair symbol, e.g. BTCUSDT.';
+    COMMENT ON COLUMN binance.klines.open_time IS 'The timestamp of the start time of the kline/candlestick data point, in milliseconds since the Unix epoch.';
+    COMMENT ON COLUMN binance.klines.open_price IS 'The opening price of the trading pair during this interval.';
+    COMMENT ON COLUMN binance.klines.high_price IS 'The highest price of the trading pair during this interval.';
+    COMMENT ON COLUMN binance.klines.low_price IS 'The lowest price of the trading pair during this interval.';
+    COMMENT ON COLUMN binance.klines.close_price IS 'The closing price of the trading pair during this interval.';
+    COMMENT ON COLUMN binance.klines.volume IS 'The trading volume during this interval.';
+    COMMENT ON COLUMN binance.klines.close_time IS 'The timestamp of the end time of the kline/candlestick data point, in milliseconds since the Unix epoch.';
+    COMMENT ON COLUMN binance.klines.quote_asset_volume IS 'The total value of the trading volume in the quote asset, which is the second asset in the trading pair (in this case, USDT).';
+    COMMENT ON COLUMN binance.klines.num_trades IS 'The number of trades that occurred during this interval.';
+    COMMENT ON COLUMN binance.klines.taker_buy_base_asset_volume IS 'The total amount of the base asset (in this case, BTC) that was bought by taker trades during this interval.';
+    COMMENT ON COLUMN binance.klines.taker_buy_quote_asset_volume IS 'The total value of the base asset volume in the quote asset (USDT) that was bought by taker trades during this interval.';
     -- Output a message indicating the table has been created
-    RAISE NOTICE 'Created binance.klines_% table with % interval', aperiod, aperiod;
 
-END
-$$;
-
-CREATE TABLE binance.kline_periods (
-                                       period VARCHAR(4) PRIMARY KEY,
-                                       duration_seconds INT
+drop table if exists binance.kline_periods;
+CREATE TABLE if not exists binance.kline_periods (
+                                       period VARCHAR(4) PRIMARY KEY
 );
 
-INSERT INTO binance.kline_periods (period, duration_seconds)
+INSERT INTO binance.kline_periods (period)
 VALUES
-    ('1m', 60),
-    ('3m', 180),
-    ('5m', 300),
-    ('15m', 900),
-    ('30m', 1800),
-    ('1h', 3600),
-    ('2h', 7200),
-    ('4h', 14400),
-    ('6h', 21600),
-    ('8h', 28800),
-    ('12h', 43200),
-    ('1d', 86400),
-    ('3d', 259200),
-    ('1w', 604800);
+    ('1m'),
+    ('3m'),
+    ('5m'),
+    ('15m'),
+    ('30m'),
+    ('1h'),
+    ('2h'),
+    ('4h'),
+    ('6h'),
+    ('8h'),
+    ('12h'),
+    ('1d'),
+    ('3d'),
+    ('1w'),
+    ('1M')
+on conflict do nothing ;
 
+CREATE TABLE IF NOT EXISTS binance.symbol_klines (
+                                                     symbol VARCHAR(20) NOT NULL,
+                                                     period VARCHAR(4) NOT NULL,
+                                                     PRIMARY KEY (symbol, period)
+);
+-- add comments on the table and columns
+COMMENT ON TABLE binance.symbol_klines
+    IS 'Table containing the symbols and periods for which we are tracking klines to generate alerts.';
+COMMENT ON COLUMN binance.symbol_klines.symbol
+    IS 'The trading symbol for which we are tracking klines.';
+COMMENT ON COLUMN binance.symbol_klines.period
+    IS 'The time period for which we are tracking klines.';
---
+++ symbol_prices.sql
@@ -0,0 +1,42 @@
+-- Stored procedure to update symbol prices and trigger alerts
+create schema if not exists binance;
+--drop table if exists binance_tickers.symbol_prices;
+
+create table if not exists binance_tickers.symbol_prices(
+    id serial primary key,
+    symbol text not null unique,
+    price float4 not null
+);
+
+drop function if exists binance.update_symbol_prices;
+CREATE OR REPLACE procedure binance.update_symbol_prices() language plpgsql AS $$
+/*
+ This is a PostgreSQL procedure that updates the prices of symbols listed on the Binance cryptocurrency exchange.
+ It does this by making an HTTP GET request to the Binance API and retrieving a JSON object that
+ contains the latest prices for all symbols.
+ The function then parses this JSON object and updates the 'symbol_prices' table in the database with the latest prices.
+
+ This function can be useful for people who want to keep track of the latest prices of all symbols available on Binance,
+ especially for those who are actively trading on the exchange.
+ However, it is important to note that this function is not suitable for real-time trading as it updates prices periodically,
+ rather than in real-time.
+
+ The procedure can be executed by calling the function name 'binance.update_symbol_prices()'
+ from within PostgreSQL or from an external application that has access to the database.
+ */
+declare
+    prices_json JSONB;
+BEGIN
+    -- Truncate symbol_prices table
+    select content::JSONB into prices_json from http_get('https://api.binance.com/api/v3/ticker/price');
+
+    -- update symbol prices
+    insert into binance.symbol_prices(symbol,price)
+    SELECT (elem->>'symbol')::TEXT, (elem->>'price')::FLOAT4
+    FROM JSONB_ARRAY_ELEMENTS(prices_json) AS elem
+    on conflict(symbol) do update set price=excluded.price;
+
+END;
+$$;
+
+call binance.update_symbol_prices();
