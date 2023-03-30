d2819449c816f3ec257d26388010f47d67401ae4
Oleksandr Kharchenko <okharch@gmail.com>
2023-03-30 14:18:59 +0300

    Create view to retrieve the list of all functions and stored procedures in the binance and trading_indicator schemas, and their parameter names and data types

--- utils.sql
+++ utils.sql
@@ -2,3 +2,15 @@ CREATE OR REPLACE FUNCTION tstz(epoch_millis bigint) RETURNS timestamptz  LANGUA
 -- Convert Unix epoch milliseconds to PostgreSQL timestamptz
 select to_timestamp(epoch_millis / 1000.0)
 $$;
+
+--  retrieve the list of all functions and stored procedures in the
+--  binance and trading_indicator schemas, and their parameter names and data types.
+create or replace view binance.sps as
+SELECT n.nspname AS schema,
+       p.proname AS function_name,
+       pg_get_function_arguments(p.oid) AS parameters,
+       CASE WHEN p.prokind = 'f' THEN 'function' ELSE 'procedure' END AS sp_type
+FROM pg_proc p
+         INNER JOIN pg_namespace n ON p.pronamespace = n.oid
+WHERE n.nspname IN ('binance', 'trading_indicator')
+ORDER BY 1, 2;
