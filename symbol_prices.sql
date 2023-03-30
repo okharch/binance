04b41d313f670bce57b1b14b2de255db21e77412
Oleksandr Kharchenko <okharch@gmail.com>
2023-03-30 09:35:05 +0300

    update_symbol_prices: enhanced line breaks for easier doc reading

--- symbol_prices.sql
+++ symbol_prices.sql
@@ -11,15 +11,16 @@ create table if not exists binance_tickers.symbol_prices(
 drop function if exists binance.update_symbol_prices;
 CREATE OR REPLACE procedure binance.update_symbol_prices() language plpgsql AS $$
 /*
- This is a PostgreSQL procedure that updates the prices of symbols listed on the Binance cryptocurrency exchange.
+ updates the prices of symbols listed on the Binance cryptocurrency exchange.
  It does this by making an HTTP GET request to the Binance API and retrieving a JSON object that
  contains the latest prices for all symbols.
- The function then parses this JSON object and updates the 'symbol_prices' table in the database with the latest prices.
+ The function then parses this JSON object and updates the 'symbol_prices' table in the database with
+ the latest prices.
 
- This function can be useful for people who want to keep track of the latest prices of all symbols available on Binance,
- especially for those who are actively trading on the exchange.
- However, it is important to note that this function is not suitable for real-time trading as it updates prices periodically,
- rather than in real-time.
+ This function can be useful for people who want to keep track of the latest prices of
+ all symbols available on Binance, especially for those who are actively trading on the exchange.
+ However, it is important to note that this function is not suitable for real-time trading as it
+ updates prices periodically, rather than in real-time.
 
  The procedure can be executed by calling the function name 'binance.update_symbol_prices()'
  from within PostgreSQL or from an external application that has access to the database.
