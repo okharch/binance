912181c0153023994ff6a9d2551c475df73ac421
Oleksandr Kharchenko <okharch@gmail.com>
2023-03-30 13:11:20 +0300

    Refactor tstz function to use SQL language and improve readability

--- utils.sql
+++ utils.sql
@@ -1,9 +1,4 @@
-CREATE OR REPLACE FUNCTION tstz(
-    epoch_millis bigint
-)
-    RETURNS timestamptz AS $$
-BEGIN
-    -- Convert Unix epoch milliseconds to PostgreSQL timestamptz
-    RETURN to_timestamp(epoch_millis / 1000.0);
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION tstz(epoch_millis bigint) RETURNS timestamptz  LANGUAGE sql AS $$
+-- Convert Unix epoch milliseconds to PostgreSQL timestamptz
+select to_timestamp(epoch_millis / 1000.0)
+$$;
