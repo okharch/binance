95cef1c1747c6c1eceb89baf13521fb27c2fcf67
Oleksandr Kharchenko <okharch@gmail.com>
2023-04-04 18:01:28 +0300

    Update get_symbol_id.sql to select symbol_id from exchange_symbols instead of id from symbol_prices

--- get_symbol_id.sql
+++ get_symbol_id.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION binance.get_symbol_id(p_symbol text)
     RETURNS integer  language SQL AS
 $QUERY$
-        SELECT id
-        FROM binance.symbol_prices
+        SELECT symbol_id
+        FROM binance.exchange_symbols
         WHERE symbol = upper(p_symbol)
 $QUERY$;
