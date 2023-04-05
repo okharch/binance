78a1620e3b867c24dac73908509183375186f843
Oleksandr Kharchenko <okharch@gmail.com>
2023-04-05 21:44:29 +0300

    update_klines_from_ws: it receives on record data->k a time, not an array ->k

--- klines/update_klines_from_ws.sql
+++ klines/update_klines_from_ws.sql
@@ -2,20 +2,23 @@ CREATE OR REPLACE FUNCTION binance.update_klines_from_ws(klines_json_text text)
     RETURNS integer AS $$
 DECLARE
     klines_json jsonb;
+    d jsonb;
+    k jsonb;
     rows_affected integer;
 BEGIN
     klines_json := klines_json_text::jsonb;
+    d := klines_json->>'data';
+    k := d->>'k';
 
     INSERT INTO binance.klines
     (symbol, period, open_time,
      open_price, high_price, low_price, close_price,
      volume, close_time, quote_asset_volume, num_trades,
      taker_buy_base_asset_volume, taker_buy_quote_asset_volume)
-    SELECT kl->>'s', kl->>'i', (kl->>'t')::bigint,
-           (kl->>'o')::numeric, (kl->>'h')::numeric, (kl->>'l')::numeric, (kl->>'c')::numeric,
-           (kl->>'v')::numeric, (kl->>'T')::bigint, (kl->>'q')::numeric, (kl->>'n')::bigint,
-           (kl->>'V')::numeric, (kl->>'Q')::numeric
-    FROM jsonb_array_elements(klines_json->'k') AS kl
+    SELECT k->>'s', k->>'i', (k->>'t')::bigint,
+           (k->>'o')::numeric, (k->>'h')::numeric, (k->>'l')::numeric, (k->>'c')::numeric,
+           (k->>'v')::numeric, (k->>'T')::bigint, (k->>'q')::numeric, (k->>'n')::bigint,
+           (k->>'V')::numeric, (k->>'Q')::numeric
     ON CONFLICT (symbol, period, open_time) DO UPDATE
         SET
             open_price = EXCLUDED.open_price,
